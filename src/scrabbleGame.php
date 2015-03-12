8302fa06a19b2409fe557fae95403eb831467ea6
erica cha & tommy bountham <student@epicodus.com>
2015-03-12 12:24:53 -0700

    This will put the array together.

--- src/scrabbleGame.php
+++ src/scrabbleGame.php
@@ -4,25 +4,36 @@
     {
         function playScrabble($input)
         {
-            if ($input == "A") {
-                return "1";
-            }
-            elseif ($input == "A,E") {
-                return "2";
-            }
-            elseif ($input == "D,G") {
-                return "4";
-            }
-            elseif ($input == "A,G"){
-                return "3";
-            }
-            elseif ($input == "T,B,Q")
-                return "24";
-            else {
-                return "Oops";
-            }
+        $string=strtolower($input);
+        $scrabble_1point = array("a","e");
+
+        if(in_array($string,$scrabble_1point))
+        {
+
+            return "it works";
         }
 
+
+        //     if ($input == "A") {
+        //         return "1";
+        //     }
+        //     elseif ($input == "A,E") {
+        //         return "2";
+        //     }
+        //     elseif ($input == "D,G") {
+        //         return "4";
+        //     }
+        //     elseif ($input == "A,G"){
+        //         return "3";
+        //     }
+        //     elseif ($input == "T,B,Q")
+        //         return "24";
+        //     else {
+        //         return "Oops";
+        //     }
+        // }
+
     }
+}
 
 ?>
